89b27d27f8baba428986a4427ebbb2f85dc6b793
Matt Klein <mklein246@gmail.com>
2019-04-26 12:30:30 -0700

    THERE. First stretch goal finished using the api's built in links. WHICH I TOTALLY KNEW ABOUT ALL ALONG AND DEFINITELY IGNORED ON PURPOSE.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -10,16 +10,14 @@ class App extends Component {
       starwarsChars: [],
       currentPage: 1,
       itemsPerPage: 10,
-      totalPages: 1
+      totalPages: 0,
+      previousPage: '',
+      nextPage: ''
     };
   }
   
   componentDidMount() {
     this.getCharacters('https://swapi.co/api/people/');
-  }
-
-  componentDidUpdate() {
-    this.totalPagesCheck();
   }
 
   getCharacters = URL => {
@@ -31,21 +29,25 @@ class App extends Component {
         return res.json();
       })
       .then(data => {
-        this.setState({ starwarsChars: data.results });
+        this.setState({
+          starwarsChars: data.results,
+          previousPage: data.previous,
+          nextPage: data.next
+        });
       })
       .catch(err => {
         throw new Error(err);
       });
   };
 
-  totalPagesCheck = () => {
-    let pages = (Math.ceil(this.state.starwarsChars.length / this.state.itemsPerPage))
-    console.log(pages)
-    if (pages === this.state.totalPages) {return}
-    this.setState({
-      totalPages: pages
-    })
-  }
+  // totalPagesCheck = () => {
+  //   let pages = (Math.ceil(this.state.starwarsChars.length / this.state.itemsPerPage))
+  //   console.log(pages)
+  //   if (pages === this.state.totalPages) {return}
+  //   this.setState({
+  //     totalPages: pages
+  //   })
+  // }
 
   pageStart = () => {
     return (
@@ -69,17 +71,21 @@ class App extends Component {
   }
 
   pageBack = () => {
-    if (this.state.currentPage <= 1) {return}
-    this.setState({
-      currentPage: this.state.currentPage - 1
-    })
+    // if (this.state.currentPage <= 1) {return}
+    // this.setState({
+    //   currentPage: this.state.currentPage - 1
+    // })
+    if (this.state.previousPage === null) {return}
+    this.getCharacters(this.state.previousPage)
   }
 
   pageForward = () => {
-    if (this.state.currentPage >= this.state.totalPages) {return}
-    this.setState({
-      currentPage: this.state.currentPage - 1
-    })
+    // if (this.state.currentPage >= this.state.totalPages) {return}
+    // this.setState({
+    //   currentPage: this.state.currentPage - 1
+    // })
+    if (this.state.nextPage === null) {return}
+    this.getCharacters(this.state.nextPage)
   }
 
   render() {
